d704a89f25348d2b311e17a3ff55eba97dda0f32
Jordan Moss <kk4jrq@gmail.com>
2018-01-09 23:02:42 -0500

    Finished rough up of Auto.  Will fine tune tomorrow.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/leaguetournamentopmodes/GlobalVars.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.leaguetournamentopmodes;
+
+/**
+ * Created by Jordan Moss on 1/7/2018.
+ */
+
+public class GlobalVars {
+    public double jewelManipulatorMiddlePosition = .5;
+    public double jewelManipulatorTopPosition = 1;
+    public double jewelManipulatorLoweredPosition = 0;
+    public double jewelManipulatorStoredPosition = .75;
+
+    public double jewelRotatorLeftPostiion = 0;
+    public double jewelRotatorMidPosition = .5;
+    public double jewelRotatorRightPosition = 1;
+    public double jewelRotatorStoredPosition = .5;
+
+    public GlobalVars(){
+
+    }
+}
